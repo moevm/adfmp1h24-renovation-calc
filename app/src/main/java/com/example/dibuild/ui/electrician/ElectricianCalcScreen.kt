9ddb68e2ba1192568619a482a18e043600094e2e
PetrBodunov <pbod@list.ru>
2024-03-05 20:53:32 +0300

    Add Electrician calc screen

--- app/src/main/java/com/example/dibuild/ui/electrician/ElectricianCalcScreen.kt
+++ app/src/main/java/com/example/dibuild/ui/electrician/ElectricianCalcScreen.kt
@@ -1,2 +1,409 @@
 package com.example.dibuild.ui.electrician
 
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.sharp.History
+import androidx.compose.material.icons.sharp.Info
+import androidx.compose.material3.AlertDialog
+import androidx.compose.material3.Button
+import androidx.compose.material3.Card
+import androidx.compose.material3.ExperimentalMaterial3Api
+import com.example.dibuild.ui.theme.DibuildTheme
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.input.ImeAction
+import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun ElectricianCalcScreen() {
+    Column(
+        verticalArrangement = Arrangement.Top,
+        horizontalAlignment = Alignment.CenterHorizontally,
+
+        ) {
+
+        Row(
+            verticalAlignment = Alignment.CenterVertically,
+            horizontalArrangement = Arrangement.SpaceBetween,
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(10.dp)
+        ) {
+            IconButton(onClick = { /*TODO*/ }) {
+                Icon(
+                    imageVector = Icons.Sharp.History,
+                    contentDescription = null,
+                    tint = Color(0xFF01ACC6),
+                    modifier = Modifier
+                        .size(50.dp)
+                )
+            }
+
+            Text(
+                text = "Электрика",
+                fontSize = 30.sp,
+            )
+
+            IconButton(
+                onClick = { /*TODO*/ }
+            ) {
+                Icon(
+                    imageVector = Icons.Sharp.Info,
+                    contentDescription = null,
+                    tint = Color(0xFF01ACC6),
+                    modifier = Modifier
+                        .size(50.dp)
+                )
+            }
+        }
+
+        Card()
+        {
+            Column(
+                horizontalAlignment = Alignment.CenterHorizontally,
+                modifier = Modifier.fillMaxWidth()
+            )
+            {
+                Text(
+                    text = "Параметры помещения",
+                    modifier = Modifier.padding(10.dp),
+                    fontSize = 30.sp
+                )
+
+                Row(
+                    verticalAlignment = Alignment.CenterVertically,
+                    modifier = Modifier.padding(vertical = 8.dp)
+                ) {
+                    Text(
+                        text = "Количество\nрозеток",
+                        fontSize = 25.sp,
+                    )
+                    Spacer(modifier = Modifier.padding(10.dp))
+                    OutlinedTextField(
+                        value = "10",
+                        onValueChange = {/*TODO*/ },
+                        keyboardOptions = KeyboardOptions.Default.copy(
+                            keyboardType = KeyboardType.Number,
+                            imeAction = ImeAction.Next
+                        ),
+                        shape = RoundedCornerShape(8.dp),
+                        modifier = Modifier.width(100.dp)
+                    )
+                    Spacer(modifier = Modifier.padding(10.dp))
+                    Text(
+                        text = "шт",
+                        fontSize = 25.sp,
+                    )
+                }
+
+                Row(
+                    verticalAlignment = Alignment.CenterVertically,
+                ) {
+                    Text(
+                        text = "Количество\nвыключателей",
+                        fontSize = 25.sp,
+                    )
+                    Spacer(modifier = Modifier.padding(10.dp))
+                    OutlinedTextField(
+                        value = "10",
+                        onValueChange = {/*TODO*/ },
+                        keyboardOptions = KeyboardOptions.Default.copy(
+                            keyboardType = KeyboardType.Number,
+                            imeAction = ImeAction.Next
+                        ),
+                        shape = RoundedCornerShape(8.dp),
+                        modifier = Modifier.width(100.dp)
+                    )
+                    Spacer(modifier = Modifier.padding(10.dp))
+                    Text(
+                        text = "шт",
+                        fontSize = 25.sp,
+                    )
+                }
+            }
+        }
+
+        Spacer(modifier = Modifier.padding(10.dp))
+
+        Card() {
+            Column(
+                horizontalAlignment = Alignment.CenterHorizontally,
+                modifier = Modifier.fillMaxWidth()
+            )
+            {
+                Text(
+                    text = "Параметры электрики",
+                    modifier = Modifier.padding(10.dp),
+                    fontSize = 30.sp
+                )
+
+                Row(
+                    verticalAlignment = Alignment.CenterVertically,
+                ) {
+                    Text(
+                        text = "Длина провода",
+                        fontSize = 25.sp,
+                    )
+                    Spacer(modifier = Modifier.padding(10.dp))
+                    OutlinedTextField(
+                        value = "2.15",
+                        onValueChange = {/*TODO*/ },
+                        keyboardOptions = KeyboardOptions.Default.copy(
+                            keyboardType = KeyboardType.Number,
+                            imeAction = ImeAction.Next
+                        ),
+                        shape = RoundedCornerShape(8.dp),
+                        modifier = Modifier.width(100.dp)
+                    )
+                    Spacer(modifier = Modifier.padding(10.dp))
+                    Text(
+                        text = "м",
+                        fontSize = 25.sp,
+                    )
+                }
+
+                Row(
+                    verticalAlignment = Alignment.CenterVertically,
+                ) {
+                    Text(
+                        text = "Цена провода",
+                        fontSize = 25.sp,
+                    )
+                    Spacer(modifier = Modifier.padding(10.dp))
+                    OutlinedTextField(
+                        value = "0.15",
+                        onValueChange = {/*TODO*/ },
+                        keyboardOptions = KeyboardOptions.Default.copy(
+                            keyboardType = KeyboardType.Number,
+                            imeAction = ImeAction.Next
+                        ),
+                        shape = RoundedCornerShape(8.dp),
+                        modifier = Modifier.width(100.dp)
+                    )
+                    Spacer(modifier = Modifier.padding(10.dp))
+                    Text(
+                        text = "₽/м",
+                        fontSize = 25.sp,
+                    )
+                }
+
+                Row(
+                    verticalAlignment = Alignment.CenterVertically,
+                ) {
+                    Text(
+                        text = "Длина\nпластикового\nкороба",
+                        fontSize = 25.sp,
+                    )
+                    Spacer(modifier = Modifier.padding(10.dp))
+                    OutlinedTextField(
+                        value = "20",
+                        onValueChange = {/*TODO*/ },
+                        keyboardOptions = KeyboardOptions.Default.copy(
+                            keyboardType = KeyboardType.Number,
+                            imeAction = ImeAction.Next
+                        ),
+                        shape = RoundedCornerShape(8.dp),
+                        modifier = Modifier.width(100.dp)
+                    )
+                    Spacer(modifier = Modifier.padding(10.dp))
+                    Text(
+                        text = "м",
+                        fontSize = 25.sp,
+                    )
+                }
+
+                Row(
+                    verticalAlignment = Alignment.CenterVertically,
+                ) {
+                    Text(
+                        text = "Цена\nпластикового\nкороба",
+                        fontSize = 25.sp,
+                    )
+                    Spacer(modifier = Modifier.padding(10.dp))
+
+                    OutlinedTextField(
+                        value = "678",
+                        onValueChange = {/*TODO*/ },
+                        keyboardOptions = KeyboardOptions.Default.copy(
+                            keyboardType = KeyboardType.Number,
+                            imeAction = ImeAction.Done
+                        ),
+                        shape = RoundedCornerShape(8.dp),
+                        modifier = Modifier.width(100.dp)
+                    )
+                    Spacer(modifier = Modifier.padding(10.dp))
+                    Text(
+                        text = "₽/м",
+                        fontSize = 25.sp,
+                    )
+                }
+
+                Row(
+                    verticalAlignment = Alignment.CenterVertically,
+                ) {
+                    Text(
+                        text = "Цена\nрозетки",
+                        fontSize = 25.sp,
+                    )
+                    Spacer(modifier = Modifier.padding(10.dp))
+
+                    OutlinedTextField(
+                        value = "678",
+                        onValueChange = {/*TODO*/ },
+                        keyboardOptions = KeyboardOptions.Default.copy(
+                            keyboardType = KeyboardType.Number,
+                            imeAction = ImeAction.Done
+                        ),
+                        shape = RoundedCornerShape(8.dp),
+                        modifier = Modifier.width(100.dp)
+                    )
+                    Spacer(modifier = Modifier.padding(10.dp))
+                    Text(
+                        text = "₽",
+                        fontSize = 25.sp,
+                    )
+                }
+
+                Row(
+                    verticalAlignment = Alignment.CenterVertically,
+                ) {
+                    Text(
+                        text = "Цена\nвыключателя",
+                        fontSize = 25.sp,
+                    )
+                    Spacer(modifier = Modifier.padding(10.dp))
+
+                    OutlinedTextField(
+                        value = "678",
+                        onValueChange = {/*TODO*/ },
+                        keyboardOptions = KeyboardOptions.Default.copy(
+                            keyboardType = KeyboardType.Number,
+                            imeAction = ImeAction.Done
+                        ),
+                        shape = RoundedCornerShape(8.dp),
+                        modifier = Modifier.width(100.dp)
+                    )
+                    Spacer(modifier = Modifier.padding(10.dp))
+                    Text(
+                        text = "₽",
+                        fontSize = 25.sp,
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun ElectricianCalcResult(){
+
+    Box(
+        contentAlignment = Alignment.Center,
+        modifier = Modifier.fillMaxSize()
+    ){
+        Card() {
+            Column(
+                horizontalAlignment = Alignment.CenterHorizontally,
+                verticalArrangement = Arrangement.SpaceBetween,
+                modifier = Modifier
+                    .padding(10.dp)
+                    .fillMaxWidth()
+            ) {
+                Text(
+                    text = "Расчёт",
+                    fontSize = 50.sp,
+                    modifier = Modifier.padding(20.dp)
+                )
+                Column(horizontalAlignment = Alignment.CenterHorizontally) {
+                    Text(
+                        text = "Стоимость провода",
+                        fontSize = 20.sp,
+                    )
+
+                    Text(
+                        text = "5000 ₽",
+                        fontSize = 20.sp,)
+                }
+
+                Spacer(modifier = Modifier.padding(20.dp))
+
+                Column(horizontalAlignment = Alignment.CenterHorizontally) {
+                    Text(text = "Стоимость короба",
+                        fontSize = 20.sp,
+                    )
+                    Text(text = "2456 ₽",
+                        fontSize = 20.sp,
+                    )
+                }
+
+                Spacer(modifier = Modifier.padding(20.dp))
+
+                Column(horizontalAlignment = Alignment.CenterHorizontally) {
+                    Text(text = "Стоимость выключателей",
+                        fontSize = 20.sp,
+                    )
+                    Text(
+                        text = "2100 ₽",
+                        fontSize = 20.sp,
+                    )
+                }
+
+                Spacer(modifier = Modifier.padding(20.dp))
+
+                Column(horizontalAlignment = Alignment.CenterHorizontally) {
+                    Text(text = "Стоимость розеток",
+                        fontSize = 20.sp,
+                    )
+                    Text(
+                        text = "3200 ₽",
+                        fontSize = 20.sp,
+                    )
+                }
+
+                Spacer(modifier = Modifier.padding(20.dp))
+
+                Column(horizontalAlignment = Alignment.CenterHorizontally) {
+                    Text(text = "Итоговая стоимость",
+                        fontSize = 20.sp,
+                    )
+                    Text(
+                        text = "12756 ₽",
+                        fontSize = 20.sp,
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Preview
+@Composable
+fun ElectricianCalcPreview(){
+    DibuildTheme {
+        ElectricianCalcResult()
+    }
+}
