79b880daa3e574311ea5440e9d314219d2172d8c
VadimArkhipov <arkhipov_vadim@inbox.ru>
2024-03-05 18:17:31 +0300

    add navbar laminate

--- app/src/main/java/com/example/dibuild/ui/laminate/LaminateCalcScreen.kt
+++ app/src/main/java/com/example/dibuild/ui/laminate/LaminateCalcScreen.kt
@@ -1,8 +1,67 @@
 package com.example.dibuild.ui.laminate
 
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.sharp.History
+import androidx.compose.material.icons.sharp.Info
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 
+@Preview
 @Composable
-fun LaminateCalcScreen(){
+fun LaminateCalcScreen() {
+    Column(
+        verticalArrangement = Arrangement.Top,
+        horizontalAlignment = Alignment.CenterHorizontally,
 
+        ) {
+
+        Row(
+            verticalAlignment = Alignment.CenterVertically,
+            horizontalArrangement = Arrangement.SpaceBetween,
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(10.dp)
+        ) {
+            IconButton(onClick = { /*TODO*/ }) {
+                Icon(
+                    imageVector = Icons.Sharp.History,
+                    contentDescription = null,
+                    tint = Color(0xFF01ACC6),
+                    modifier = Modifier
+                        .size(50.dp)
+                )
+            }
+
+            Text(
+                text = "Ламинат",
+                fontSize = 30.sp,
+            )
+
+            IconButton(
+                onClick = { /*TODO*/ }
+            ) {
+                Icon(
+                    imageVector = Icons.Sharp.Info,
+                    contentDescription = null,
+                    tint = Color(0xFF01ACC6),
+                    modifier = Modifier
+                        .size(50.dp)
+                )
+            }
+        }
+    }
 }
